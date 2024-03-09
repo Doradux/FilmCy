8a5ed82018e88d9ba80bdada8937cc6dae33e0ee
Doradux <marcosloldorado@gmail.com>
2024-03-10 00:41:02 +0100

    se anaden algunas caracteristicas de la plataforma y el footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
       colors: {
         "main-color": "#fe0b21",
         "main-hover": "#880713",
+        "gray": "#414247",
       },
       backgroundImage: {
         'hero': "linear-gradient(to bottom, rgba(0,0,0,0.8), rgba(0,0,0,0.6)), url('../../src/assets/hero.png')",
