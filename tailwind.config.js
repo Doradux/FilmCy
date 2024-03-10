a309a0a7d3979fd991b85a5159a026b1976beea6
Doradux <marcosloldorado@gmail.com>
2024-03-10 06:30:20 +0100

    se crea la pagina de vista de series y peliculas

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {
       colors: {
         "main-color": "#fe0b21",
         "main-hover": "#880713",
-        "gray": "#414247",
+        gray: "#414247",
       },
       backgroundImage: {
-        'hero': "linear-gradient(to bottom, rgba(0,0,0,0.8), rgba(0,0,0,0.6)), url('../../src/assets/index/hero.png')",
-        'registermobile': "linear-gradient(to bottom, rgba(0,0,0,0.8), rgba(0,0,0,0.6)), url('../../src/assets/register/bg.webp')",
-        'register': "linear-gradient(to bottom, rgba(0,0,0,0.8), rgba(0,0,0,0.6)), url('../../src/assets/register/bg.png')",
-        'tv': "linear-gradient(to bottom, rgba(0,0,0,0.8), rgba(0,0,0,0.8)), url('../../src/assets/index/tv.webp')",
-      }
+        hero: "linear-gradient(to bottom, rgba(0,0,0,0.8), rgba(0,0,0,0.6)), url('../../src/assets/index/hero.png')",
+        registermobile:
+          "linear-gradient(to bottom, rgba(0,0,0,0.8), rgba(0,0,0,0.8)), url('../../src/assets/register/bgmobile.png')",
+        register:
+          "linear-gradient(to bottom, rgba(0,0,0,0.8), rgba(0,0,0,0.6)), url('../../src/assets/register/bg.png')",
+        tv: "linear-gradient(to bottom, rgba(0,0,0,0.8), rgba(0,0,0,0.8)), url('../../src/assets/index/tv.webp')",
+      },
     },
   },
-  plugins: [],
-}
+  plugins: [require("flowbite/plugin")],
+};
