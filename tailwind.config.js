ecb390456aff39e72db8245edfede3459d692252
Doradux <marcosloldorado@gmail.com>
2024-03-10 13:59:13 +0100

    se termina la pagina de seleccion de peliculas o series

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,9 @@ module.exports = {
         register:
           "linear-gradient(to bottom, rgba(0,0,0,0.8), rgba(0,0,0,0.6)), url('../../src/assets/register/bg.png')",
         tv: "linear-gradient(to bottom, rgba(0,0,0,0.8), rgba(0,0,0,0.8)), url('../../src/assets/index/tv.webp')",
+        grad: "linear-gradient(90deg, rgba(0,0,0,1) 0%, rgba(0,0,0,0.6) 35%, rgba(15,10,4,0) 80%, rgba(252,176,69,0) 100%)",
+        movieselected: "linear-gradient(to bottom, rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url('../../src/assets/moviesseries/exclusives/lopeinfo.webp')",
+        blur: "linear-gradient(to bottom, rgba(0,0,0,0.8), rgba(0,0,0,0.8))",
       },
     },
   },
